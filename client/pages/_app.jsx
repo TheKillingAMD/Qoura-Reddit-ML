c621c339cce5df63a8817ce7950c601734eecc8b
Niraj Nandish <nirajnandish@icloud.com>
2021-04-26 13:26:03 +0400

    Add next-auth provider

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,7 +1,13 @@
+import { Provider } from 'next-auth/client';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
 
 export default MyApp;
